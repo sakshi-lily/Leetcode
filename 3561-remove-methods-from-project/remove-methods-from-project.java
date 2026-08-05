f4b0a383a7763388fb8312170e528405dd733f0e
Sakshi <sakshidk1502@gmail.com>
2026-08-05 10:01:13 +0530

    Time: 76 ms (54.81%) | Memory: 293.8 MB (21.16%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.java
+++ 3561-remove-methods-from-project/remove-methods-from-project.java
@@ -4,20 +4,14 @@ class Solution {
         for (int i = 0; i < n; i++) {
             graph[i] = new ArrayList<>();
         }
-
         for (int[] edge : invocations) {
             graph[edge[0]].add(edge[1]);
         }
-
         boolean[] suspicious = new boolean[n];
-
         dfs(k, graph, suspicious);
-
-        // Check if any non-suspicious method calls a suspicious method
         for (int[] edge : invocations) {
             int u = edge[0];
             int v = edge[1];
-
             if (!suspicious[u] && suspicious[v]) {
                 List<Integer> ans = new ArrayList<>();
                 for (int i = 0; i < n; i++) {
@@ -26,22 +20,17 @@ class Solution {
                 return ans;
             }
         }
-
         List<Integer> ans = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             if (!suspicious[i]) {
                 ans.add(i);
             }
         }
-
         return ans;
     }
-
     private void dfs(int node, List<Integer>[] graph, boolean[] suspicious) {
         if (suspicious[node]) return;
-
         suspicious[node] = true;
-
         for (int next : graph[node]) {
             dfs(next, graph, suspicious);
         }
